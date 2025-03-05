516be6ff33e426cd80b096aa74b2f0d91771c1ab
mayur.popli <mayur.popli@thewitslab.com>
2025-03-05 17:27:52 +0530

    fix: client Services

--- cmd/clientSideHandler/main.go
+++ cmd/clientSideHandler/main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	// Standard library packages
 	"context"
 	"flag"
 	"fmt"
 	"net/http"
 	"os"
 
-	// BecknOnix packages
 	log "beckn-onix/log"
-
-	// Third-party packages
-	"gopkg.in/yaml.v2" // For unmarshaling YAML
+	"gopkg.in/yaml.v2"
 )
 
 type config struct {
@@ -28,13 +24,13 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 
 	log.Log.Info("Received request:", r.Method, r.URL.Path, r.Header)
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, "Message received successfully")
 }
 
 func run(ctx context.Context, configPath string) error {
 	configuration, err := initConfig(ctx, configPath)
 	if err != nil {
-		return fmt.Errorf("error initializing config: %w", err)
+		log.Log.Error("error initializing config: ", err)
+		return err
 	}
 
 	port := fmt.Sprintf(":%d", configuration.Port)
@@ -42,7 +38,7 @@ func run(ctx context.Context, configPath string) error {
 
 	server := &http.Server{Addr: port}
 
-	// Run server in a goroutine
+	// Run server in a goroutine.
 	go func() {
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Log.Error("Server failed:", err)
@@ -57,7 +53,8 @@ func run(ctx context.Context, configPath string) error {
 func initConfig(ctx context.Context, path string) (*config, error) {
 	file, err := os.Open(path)
 	if err != nil {
-		return nil, fmt.Errorf("could not open config file: %v", err)
+		log.Log.Error("could not open config file: ", err)
+		return nil, err
 	}
 	defer file.Close()
 
@@ -65,7 +62,8 @@ func initConfig(ctx context.Context, path string) (*config, error) {
 	decoder := yaml.NewDecoder(file)
 	err = decoder.Decode(&config)
 	if err != nil {
-		return nil, fmt.Errorf("could not unmarshal config data: %v", err)
+		log.Log.Error("could not unmarshal config data: ", err)
+		return nil, err
 	}
 	if config.AppName == "" || config.Port == 0 {
 		return nil, fmt.Errorf("missing required fields in config")
@@ -81,6 +79,6 @@ func main() {
 	flag.Parse()
 
 	if err := run(context.Background(), configPath); err != nil {
-		log.Log.Error("Application failed:", err)
+		log.Log.Error("Application failed:", err) //TODO: change to fatal
 	}
 }
