49a4532929a67d4612d2ae3606004038cdde8fe5
tanyamadaan <tanyamadaan20@gmail.com>
2025-04-03 16:08:57 +0530

    Bug Fix for ReqPreProcessor plugin

--- pkg/plugin/implementation/reqpreprocessor/cmd/plugin.go
+++ pkg/plugin/implementation/reqpreprocessor/cmd/plugin.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"net/http"
-	"strings"
 
 	"github.com/beckn/beckn-onix/pkg/plugin/implementation/reqpreprocessor"
 )
@@ -12,9 +11,6 @@ type provider struct{}
 
 func (p provider) New(ctx context.Context, c map[string]string) (func(http.Handler) http.Handler, error) {
 	config := &reqpreprocessor.Config{}
-	if contextKeysStr, ok := c["contextKeys"]; ok {
-		config.ContextKeys = strings.Split(contextKeysStr, ",")
-	}
 	if role, ok := c["role"]; ok {
 		config.Role = role
 	}
