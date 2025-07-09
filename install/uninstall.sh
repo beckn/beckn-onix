45e83024401f8a86d0d2c5525385deb027443a6e
shreyvishal <shreya.vishal@eminds.ai>
2025-07-09 13:45:30 +0530

    Added: Changes for private key issue

--- install/uninstall.sh
+++ install/uninstall.sh
@@ -1,7 +1,7 @@
-docker-compose -f docker-compose-bap.yml down -v
-docker-compose -f docker-compose-bpp.yml down -v
-docker-compose -f docker-compose-bpp-with-sandbox.yml down -v
-docker-compose -f docker-compose-gateway.yml down -v
-docker-compose -f docker-compose-registry.yml down -v
-docker-compose -f docker-compose-app.yml down -v
+docker compose -f docker-compose-bap.yml down -v
+docker compose -f docker-compose-bpp.yml down -v
+docker compose -f docker-compose-bpp-with-sandbox.yml down -v
+docker compose -f docker-compose-gateway.yml down -v
+docker compose -f docker-compose-registry.yml down -v
+docker compose -f docker-compose-app.yml down -v
 docker volume rm registry_data_volume registry_database_volume registry_logs_volume gateway_data_volume gateway_database_volume bap_client_config_volume bap_network_config_volume bpp_client_config_volume bpp_network_config_volume
