bfbc6451231b8e1884fd9f82b4cf758c71a4106c
Mayuresh <mayuresh@becknprotocol.io>
2026-07-08 09:25:37 +0530

    fix(schemaversionmediator): guard nil To pointer in incompatibleType debug log

--- pkg/plugin/implementation/schemaversionmediator/schemaversionmediator.go
+++ pkg/plugin/implementation/schemaversionmediator/schemaversionmediator.go
@@ -427,7 +427,11 @@ func (m *mediator) Mediate(ctx *model.StepContext) error {
 	}
 	log.Debugf(ctx, "schemaversionmediator: compatibilityCheck counterparty=%q result=incompatible needs=%d", counterpartyID, len(needs))
 	for _, n := range needs {
-		log.Debugf(ctx, "schemaversionmediator: incompatibleType counterparty=%q type=%q fromContext=%q toContext=%q", counterpartyID, n.From.Type, n.From.ContextURL, n.To.ContextURL)
+		toContext := "<nil>"
+		if n.To != nil {
+			toContext = n.To.ContextURL
+		}
+		log.Debugf(ctx, "schemaversionmediator: incompatibleType counterparty=%q type=%q fromContext=%q toContext=%q", counterpartyID, n.From.Type, n.From.ContextURL, toContext)
 	}
 
 	if m.policy.Action == PolicyActionReject {
